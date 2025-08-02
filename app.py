307c4e49cbee0ec84b872882f075aeac1b2fb058
Pothireddy Lavanya <156573135+lavanyapothireddy@users.noreply.github.com>
2025-08-02 21:42:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import    Flask, render_template, request, jsonify
 import os
 import numpy as np
 from tensorflow.keras.models import load_model
